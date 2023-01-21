026c7edfe13d2a703b82f9023c1d662a5efa7fd9
الجوكر الفلسطيني <107787017+Jokergazaa@users.noreply.github.com>
2023-01-21 16:14:36 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#code_recorded_by_tech_cochi
+clear
+sleep 1
+echo ""
+echo ""
+echo " ██████  ╝██╔════╝██╔═   ██
+░░░██║░░░█████╗░░██║░░╚═╝███████║
+░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+░░░██║░░░███████╗╚████  ╝██║░░██║
+░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝ " | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 1
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 1
+clear
+echo " done " | lolcat
+sleep 1
+clear
+unzip techcochi.zip
+clear
+rm -rf techcochi.zip
+clear
+bash about.sh
+sleep 4
+echo ""
+echo ""
+echo " Follow me on Facebook 🔔 " | lolcat
+xdg-open https://www.facebook.com/JOGeeR.plstaeeen/
+echo ""
+sleep 10
+echo " create account on cashkaro.com "
+echo " set your browser "
+termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
+echo ""
